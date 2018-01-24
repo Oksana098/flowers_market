ed769f764427bfbf88b09544badddc6459a8b286
oksana-pidhurska <oksana.pidhurska@gmail.com>
2018-01-24 09:25:36 -0500

    create models

--- site_flowers/models.py
+++ site_flowers/models.py
@@ -2,4 +2,16 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Flowers(models.Model):
+    flowers_title = models.CharField(max_length=300)
+    color_flowers = models.CharField(max_length=200)
+    description_flowers = models.TextField()
+
+
+class FlowersPrice(models.Model):
+    flowers = models.ForeignKey(Flowers)
+    flowers_price = models.IntegerField()
+    discount = models.IntegerField()
+
+
