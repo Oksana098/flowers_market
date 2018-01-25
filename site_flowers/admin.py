90df2ed12210fff8de009fb312dc708813e2ba6e
oksana-pidhurska <oksana.pidhurska@gmail.com>
2018-01-25 04:45:08 -0500

    improvement model

--- site_flowers/admin.py
+++ site_flowers/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Flowers, FlowersPrice
 
-# Register your models here.
+
+class FlowersDashboard(admin.ModelAdmin):
+    list_display = ('flowers_name', 'color_flowers', 'type')
+    search_fields = ('flowers_name', 'color_flowers', 'type')
+
+
+admin.site.register(Flowers, FlowersDashboard)
+admin.site.register(FlowersPrice)
+
--- site_flowers/models.py
+++ site_flowers/models.py
@@ -2,11 +2,27 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+CHOICES = (
+    ('1', 'Roze'),
+    ('2', 'Tulip')
+)
+
 
 class Flowers(models.Model):
-    flowers_title = models.CharField(max_length=300)
-    color_flowers = models.CharField(max_length=200)
-    description_flowers = models.TextField()
+    flowers_name = models.CharField(max_length=255, unique=True)
+    color_flowers = models.CharField(max_length=255)
+    description = models.TextField(max_length=500)
+    image = models.ImageField()
+    date = models.DateField()
+    code = models.CharField(max_length=50, unique=True)
+    type = models.CharField(max_length=50, choices=CHOICES)
+
+    class Meta:
+        verbose_name = 'Flower'
+        verbose_name_plural = 'Flowers'
+
+    def __unicode__(self):
+        return '{}'.format(self.flowers_name)
 
 
 class FlowersPrice(models.Model):
@@ -14,4 +30,9 @@ class FlowersPrice(models.Model):
     flowers_price = models.IntegerField()
     discount = models.IntegerField()
 
+    class Meta:
+        verbose_name = 'FlowerPrice'
+        verbose_name_plural = 'Flowers Prices'
 
+    def __unicode__(self):
+        return '{}'.format(self.flowers_price)
